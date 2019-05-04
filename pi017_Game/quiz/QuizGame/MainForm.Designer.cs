fedf6c880be109354cab17a22d5200d3d3a95d8c
sergey verevkin <sergeyverevkin@gmail.com>
2019-05-04 09:19:21 +0700

    misfiles

---
+++ pi017_Game/quiz/QuizGame/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp1
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.tabPage3 = new System.Windows.Forms.TabPage();
+      this.tabPage4 = new System.Windows.Forms.TabPage();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.button2 = new System.Windows.Forms.Button();
+      this.tabControl1.SuspendLayout();
+      this.tabPage1.SuspendLayout();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Controls.Add(this.tabPage3);
+      this.tabControl1.Controls.Add(this.tabPage4);
+      this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tabControl1.Location = new System.Drawing.Point(0, 0);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(555, 251);
+      this.tabControl1.TabIndex = 2;
+      this.tabControl1.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl1_Selecting);
+      // 
+      // tabPage1
+      // 
+      this.tabPage1.Controls.Add(this.button1);
+      this.tabPage1.Controls.Add(this.textBox1);
+      this.tabPage1.Location = new System.Drawing.Point(4, 22);
+      this.tabPage1.Name = "tabPage1";
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage1.Size = new System.Drawing.Size(547, 225);
+      this.tabPage1.TabIndex = 0;
+      this.tabPage1.Text = "tabPage1";
+      this.tabPage1.UseVisualStyleBackColor = true;
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(547, 257);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "tabPage2";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      // 
+      // tabPage3
+      // 
+      this.tabPage3.Location = new System.Drawing.Point(4, 22);
+      this.tabPage3.Name = "tabPage3";
+      this.tabPage3.Size = new System.Drawing.Size(547, 257);
+      this.tabPage3.TabIndex = 2;
+      this.tabPage3.Text = "tabPage3";
+      this.tabPage3.UseVisualStyleBackColor = true;
+      // 
+      // tabPage4
+      // 
+      this.tabPage4.Location = new System.Drawing.Point(4, 22);
+      this.tabPage4.Name = "tabPage4";
+      this.tabPage4.Size = new System.Drawing.Size(547, 257);
+      this.tabPage4.TabIndex = 3;
+      this.tabPage4.Text = "tabPage4";
+      this.tabPage4.UseVisualStyleBackColor = true;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(8, 19);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(226, 84);
+      this.textBox1.TabIndex = 2;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(252, 19);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(156, 23);
+      this.button1.TabIndex = 3;
+      this.button1.Text = "Подсмотреть вопрос";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // panel1
+      // 
+      this.panel1.Controls.Add(this.button2);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(0, 251);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(555, 32);
+      this.panel1.TabIndex = 4;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(199, 5);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(156, 23);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "Переключение";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(555, 283);
+      this.Controls.Add(this.tabControl1);
+      this.Controls.Add(this.panel1);
+      this.Name = "MainForm";
+      this.Text = "Form1";
+      this.tabControl1.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
+      this.tabPage1.PerformLayout();
+      this.panel1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TabControl tabControl1;
+    private System.Windows.Forms.TabPage tabPage1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TabPage tabPage2;
+    private System.Windows.Forms.TabPage tabPage3;
+    private System.Windows.Forms.TabPage tabPage4;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Button button2;
+  }
+}
+
---
+++ pi017_Game/quiz/QuizGame/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.GameServiceReference;
+
+namespace WindowsFormsApp1
+{
+
+  public partial class MainForm : Form
+  {
+    private GameClient m_pClient;
+    public MainForm()
+    {
+      InitializeComponent();
+
+      string sAddress = "http://127.0.0.1:8000/Service";
+      m_pClient = new GameClient(
+        new BasicHttpBinding(),
+        new EndpointAddress(sAddress));
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        CQuestion pQ = m_pClient.GetQuestion();
+        textBox1.Text = pQ.Text;
+      }
+      catch (Exception pE)
+      {
+        textBox1.Text = pE.Message;
+      }
+    }
+
+    private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+    {
+      //if (e.Action == TabControlAction.Deselecting)
+      //{
+      //  e.Cancel = true;
+      //}
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      int iIndex = 1;
+      // 
+      iIndex = tabControl1.SelectedIndex + 1;
+      if (iIndex >= tabControl1.TabCount)
+      {
+        iIndex = 0;
+
+      }
+      tabControl1.SelectedIndex = iIndex;
+    }
+  }
+}
